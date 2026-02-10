57a0acafefb2461fc672b8b3dbc64cd95515b131
Abhay0506 <168776158+jangirabhay@users.noreply.github.com>
2026-02-10 23:08:37 +0530

    Reorder medRelief and medAvoid fields in Medicine schema

--- models/Medicine.js
+++ models/Medicine.js
@@ -23,11 +23,12 @@ const medicineSchema = new mongoose.Schema({
   medImage: { type: String },
   medHelps: { type: [String] },
   // medWork: { type: [String] },
-  medRelief: {type: String},
   medForm: medFormSchema,
   medDosage: [dosageSchema],
   medTake: { type: [String] },
-  medAvoid: {type : [String]}
+  medAvoid: {type : [String]},
+  medRelief: { type: String }
+
 }, { timestamps: true });
 
 module.exports = mongoose.model('Medicine', medicineSchema);
